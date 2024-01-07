d7e037d7c89297e52bfef1d0c31f552febb4fedc
Valera415 <antrnt311@gmail.com>
2024-01-08 00:38:19 +0300

    bimbimbambam

---
+++ siteblog/blog/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'slug': self.slug})
+
+    class Meta:
+        ordering = ['title']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('tag', kwargs={'slug': self.slug})
+
+    class Meta:
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, verbose_name='Url', unique=True)
+    author = models.CharField(max_length=100)
+    content = models.TextField(max_length=1000, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
+    photo = models.ImageField(upload_to='photos/%Y/%m')
+    views = models.IntegerField(default=0, verbose_name='Просмотры')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'slug': self.slug})
+
+    class Meta:
+        ordering = ['-created_at']
+
+
